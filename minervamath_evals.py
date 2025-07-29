b4e3650f8652f246852ae9306462c3437a4fc677
Junho Na <79076958+junho328@users.noreply.github.com>
2025-07-29 13:57:47 +0900

    <DELETE> 4 samples

--- minervamath_evals.py
+++ minervamath_evals.py
@@ -33,7 +33,7 @@ task = LightevalTaskConfig(
     generation_size=32768,
     metric=[
         Metrics.math_pass_at_1_1n,
-        Metrics.math_pass_at_1_4n,
+        # Metrics.math_pass_at_1_4n,
         # Metrics.math_pass_at_1_8n,
         # Metrics.math_pass_at_1_16n,
         # Metrics.math_pass_at_1_32n,
